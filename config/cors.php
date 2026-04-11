deddf9ab5bb25b1732882d06b7ed144ae88c2bf8
snowZ <elgarouani01@gmail.com>
2026-04-11 02:11:57 +0100

    fix: restore CORS vercel wildcard

--- config/cors.php
+++ config/cors.php
@@ -1,20 +1,17 @@
 <?php
 
 return [
-    'paths'                    => ['api/*', 'sanctum/csrf-cookie'],
-    'allowed_methods'          => ['*'],
-    'allowed_origins'          => [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [
         env('FRONTEND_URL', 'http://localhost:5173'),
-        'http://localhost:5173',
-        'http://localhost:5174',
-        'http://localhost:5175',
         'http://localhost:3000',
     ],
     'allowed_origins_patterns' => [
-        '#^https?://.*\.vercel\.app$#' // Dynamically allow any vercel deployment
+        '#^https?://.*\.vercel\.app$#'
     ],
-    'allowed_headers'          => ['*'],
-    'exposed_headers'          => [],
-    'max_age'                  => 0,
-    'supports_credentials'     => true,
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => true,
 ];
